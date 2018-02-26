b68bda3d98af3754216a90a13aa5ea7fd97fc719
fogelkuba <fogelkuba@gmail.com>
2018-02-27 00:03:35 +0100

    l5

--- index.php
+++ index.php
@@ -1,15 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-<body>
-    <?php
-        echo "<p>lekcja 2</p>";
-     ?>
+<?php
+    // komentarze
+    echo "<p>Zmienne</p>";
+    echo "hello" ;
 
-</body>
-</html>
+
+ ?>
