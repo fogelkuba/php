35f604928f3e93e23f78915e2c4628f44ddab228
fogelkuba <fogelkuba@gmail.com>
2018-03-03 20:16:36 +0100

    fix

---
+++ operatory-warunkowe.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Operatory Warunkowe</title>
+</head>
+<body>
+    <?php
+    echo "<h1>Operatory Warunkowe</h1>";
+
+    $a = 20;
+    $b = 30;
+
+    $msg = ($a > $b) ? "a jest wieksze od b" : "a jest mniejsze lub równe b";
+    echo $msg;
+    echo ($a > $b) ? $a : $b;
+
+
+     ?>
+</body>
+</html>
