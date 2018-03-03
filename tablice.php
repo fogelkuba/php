6c59699d33a0ba712bc3f1cad984163dd13f9afa
fogelkuba <fogelkuba@gmail.com>
2018-03-03 16:31:59 +0100

    fix

--- tablice.php
+++ tablice.php
@@ -8,7 +8,25 @@
 </head>
 <body>
     <?php
-        
+        $tab[0] = "zero";
+        $tab[1] = 10;
+        $tab[2] = 20;
+        $tab[3] = 30;
+        $tab[4] = 40;
+        echo $tab.":"."<br>";
+        echo $tab[0];
+        echo $tab[1];
+        echo $tab[2];
+        echo $tab[3];
+        echo $tab[4];
+
+        $tab[] = 50;
+        echo $tab[5];
+
+        echo "<h2> print_r($tab) </h2>";
+        print_r($tab);
+        echo "<p> drukuje zawartość tablicy </p>";
+
      ?>
 
 </body>
