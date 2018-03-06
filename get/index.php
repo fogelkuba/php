31ac38352634c1f947bac7d876872a974d724a52
fogelkuba <fogelkuba@gmail.com>
2018-03-06 21:22:03 +0100

    fix

--- get/index.php
+++ get/index.php
@@ -1,31 +1,30 @@
 <html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        
         <?php
                 if (isset($_GET['page']))
                 {
                     $allowed_pages = array("java", "php", "mysql");
 
-                    $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING);           
+                    $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING);
 
                     if (!empty($page))
                     {
-                        if (in_array($page, $allowed_pages))                       
+                        if (in_array($page, $allowed_pages))
                         {
                             if (is_file($page.".php"))
-                                include($page.".php");              
+                                include($page.".php");
                         }
-                    }                   
+                    }
                 }
-                else                
-                    include ("start.php");                  
-                
-                    
+                else
+                    include ("start.php");
+
+
                 echo "<title>$title</title>";
         ?>
-        
-        
+
+
         <link rel="stylesheet" href="style.css" type="text/css" />
         <script type="text/javascript" src="jsc.js"></script>
 
@@ -35,21 +34,21 @@
             <div id="top">
                 BANER
             </div>
-            
+
             <div id="menu">
                 <div><a href="?page=java">Java</a></div>
                 <div><a href="?page=php">PHP</a></div>
                 <div><a href="?page=mysql">MySql</a></div>
             </div>
-            <div id="content">     
+            <div id="content">
                 <div id="text">
         <?php
         if (isset($_GET['page']))
         {
             $allowed_pages = array("java", "php", "mysql");
-            
-            $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING);           
-            
+
+            $page = filter_var($_GET['page'], FILTER_SANITIZE_STRING);
+
             if (!empty($page))
             {
                 if (!in_array($page, $allowed_pages))
@@ -62,9 +61,9 @@
                         echo "Taka strona nie istnieje";
                 }
             }
-            
+
         }
-        else  
+        else
            include("start.html");
 
         ?>
@@ -74,4 +73,3 @@
         </div>
     </body>
 </html>
-
