666be2d996a466b3353528d8e01749d311178d7c
fogelkuba <fogelkuba@gmail.com>
2018-03-14 21:47:16 +0100

    fix

--- post/odbierz.php
+++ post/odbierz.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>POST LOGOWANIE</title>
 </head>
 <body>
 
@@ -13,8 +13,16 @@
         if (isset($_POST['login']) && isset($_POST['pass'])) {
 
             if (!empty($_POST['login']) && !empty($_POST['pass'])) {
-                echo 'witaj '.$_POST['login'];
-                echo "<a href='/php/post'>back</a>";
+                $user = filter_var($_POST['login'], FILTER_SANITIZE_STRING);
+                $pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
+
+                if ($user === 'kubu' ) {
+                    echo 'witaj kubu supermistrzu';
+                }else{
+                    echo "Witaj $user";
+
+                }
+                echo "<br/>"."<a href='/php/post'>back</a>";
             }else{
                 echo "brak użytkownika! spróbuj ponownie <a href='/php/post'>tutaj</a>";
             }
