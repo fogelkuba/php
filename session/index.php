0d6ba9d74ac1bca74601f4e82302c868be210ca2
fogelkuba <fogelkuba@gmail.com>
2018-03-17 13:25:57 +0100

    fix

--- session/index.php
+++ session/index.php
@@ -2,6 +2,17 @@
     session_start(); //$_SESSION['nazwa'] = wartość;
 ?>
 
+
+<!-- logout -->
+<?php
+    if (isset($_GET['akcja']) && $_GET['akcja'] == 'wyloguj') {
+        $_SESSION['zalogowany'] = 0;
+        echo 'zostałeś wylogowany koleżko';
+        // echo '<a href="index.php"> Wróć do strony głównej</a>';
+        session_destroy();
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="pl">
 <head>
@@ -16,42 +27,29 @@
         if ($_SESSION['zalogowany'] != 1) {
             include('login.php');
         }
-    ?>
-
-    <hr>
-    <h2>
+     ?>
 
     <?php
-        if (isset($_GET['akcja']) && $_GET['akcja'] == 'wyloguj') {
-            $_SESSION['zalogowany'] = 0;
-            echo 'zostałeś wylogowany koleżko';
-            echo '<a href="index.php"> Wróć do strony głównej</a>';
-            session_destroy();
+        if ((isset($_POST['login']) && isset($_POST['pass'])) || $_SESSION['zalogowany'] == 1) {
 
-        }else{
-            if ((isset($_POST['login']) && isset($_POST['pass'])) || $_SESSION['zalogowany'] == 1) {
+            if ((!empty($_POST['login']) && !empty($_POST['pass'])) || $_SESSION['zalogowany'] == 1  ) {
+                $user = filter_var($_POST['login'], FILTER_SANITIZE_STRING);
+                $pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
 
-                if ((!empty($_POST['login']) && !empty($_POST['pass'])) || $_SESSION['zalogowany'] == 1  ) {
-                    if ($_SESSION['zalogowany'] == 0) {
-                        $user = filter_var($_POST['login'], FILTER_SANITIZE_STRING);
-                        $pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
-                    }
-
-                    if (($user === 'admin' ) || $_SESSION['zalogowany'] == 1 ) {
-                        include('admin-panel.php');
-                    }else{
-                        echo "Witaj $user";
-                    }
-
+                if (($user === 'admin' ) || $_SESSION['zalogowany'] == 1 ) {
+                    include('admin-panel.php');
                 }else{
-                    echo "brak użytkownika! spróbuj ponownie";
+                    echo "Witaj $user";
                 }
+
             }else{
-                echo "brak użytkownika! spróbuj ponownie!";
+                echo "brak użytkownika! spróbuj ponownie";
             }
+        }else{
+            // echo "brak użytkownika! spróbuj ponownie!";
         }
      ?>
-</h2>
+
 
 
 </body>
