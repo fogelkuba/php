17b9ee2ea36cf7bdad69d7b5c9eef48ebc7c7669
fogelkuba <fogelkuba@gmail.com>
2018-03-25 18:43:11 +0200

    fix

--- programowanie-obiektowe/class.Time.php
+++ programowanie-obiektowe/class.Time.php
@@ -1,10 +1,19 @@
 <?php
 
-date_default_timezone_set("Europe/Warsaw");
 
 class Time {
+    const DEFAULT_TIME_ZONE = "Europe/Warsaw";
     public $currentTime;
     public $futureTime;
+
+    function __construct(){
+        date_default_timezone_set(DEFAULT_TIME_ZONE);
+        $this->currentTime = $this->getCurrentTime();
+    }
+
+    function __tostring(){
+        echo $this->currentTime;
+    }
 
     function setFutureTime($days, $pattern = "H:i:s d M Y"){
         $this->futureTime = date($pattern, strtotime("+ $days days"))."<br>";
