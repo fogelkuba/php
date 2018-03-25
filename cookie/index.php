777fb07c081f7cd28846718b807f50fe386002e8
fogelkuba <fogelkuba@gmail.com>
2018-03-25 12:58:53 +0200

    fix

--- cookie/index.php
+++ cookie/index.php
@@ -3,6 +3,11 @@
     session_start();
     //$_SESSION['nazwa'] = wartość;
 
+    if (isset($_GET['ref'])) {
+        $ref = filter_var($_GET['ref'], FILTER_SANITIZE_STRING);
+        
+    }
+
     if (!isset($_SESSION['initiate'])) {
 
         session_regenerate_id();
