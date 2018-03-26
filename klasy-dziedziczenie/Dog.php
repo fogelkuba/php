c1a10a0be0a6120e8643ff034c29614a4e7c914a
fogelkuba <fogelkuba@gmail.com>
2018-03-26 22:32:35 +0200

    fix

--- klasy-dziedziczenie/Dog.php
+++ klasy-dziedziczenie/Dog.php
@@ -6,7 +6,12 @@ class Dog extends AnimalAbstract{
         $this->isHungry = $isHungry;
     }
     function eat(){
-
+        if ($isHungry == 'true') {
+            echo "eating"."<br>";
+            $this->isHungry = false;
+        }else{
+            echo "i'm not hungry"."<br>";
+        }
     }
 }
 
