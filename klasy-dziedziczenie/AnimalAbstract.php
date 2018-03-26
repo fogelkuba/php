29a3491156214a9f66d3a52a552179e308890c12
fogelkuba <fogelkuba@gmail.com>
2018-03-26 22:11:58 +0200

    fix

--- klasy-dziedziczenie/AnimalAbstract.php
+++ klasy-dziedziczenie/AnimalAbstract.php
@@ -1,7 +1,7 @@
 <?php
 
 abstract class AnimalAbstract{
-    function eat($food){
+    abstract function eat(){
 
     }
 }
--- klasy-dziedziczenie/Dog.php
+++ klasy-dziedziczenie/Dog.php
@@ -1,7 +1,13 @@
 <?php
 
 class Dog extends AnimalAbstract{
-    
+    protected $isHungry;
+    function __construct($isHungry = true){
+        $this.isHungry = $isHungry;
+    }
+    function eat(){
+
+    }
 }
 
  ?>
