3a38f84ea7f052bb986f7332717a0a5aea69b620
fogelkuba <fogelkuba@gmail.com>
2018-03-28 22:21:11 +0200

    fix

--- klasy-dziedziczenie/Punkt3d.php
+++ klasy-dziedziczenie/Punkt3d.php
@@ -1,8 +1,18 @@
 <?php
 
 class Punkt3d extends Punkt2d{
+
+    public $z;
+
     public function __construct($x = 0, $y = 0, $z = 0){
-        $this->x;
+        parent::__construct($x, $y);
+        $this->z = $z;
+    }
+    public function setZ($z){
+        $this->z = $z;
+    }
+    public function getZ(){
+        return $this->z;
     }
 }
  ?>
