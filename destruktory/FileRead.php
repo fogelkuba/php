b8282905d85a44d13a92a54ecaf56695883a9d47
fogelkuba <fogelkuba@gmail.com>
2018-04-01 18:07:32 +0200

    fix

--- destruktory/FileRead.php
+++ destruktory/FileRead.php
@@ -2,9 +2,17 @@
     class FileRead {
 
         protected $filepointer;
+        protected $filename;
 
         function __construct($filename){
-            $this->filepointer = @fopen($filename, "r");
+            $this->filename = $filename;
+            if (!($this->filepointer = @fopen($filename, "r"))) {
+                echo " nie ma takiego pliku"."<br>";
+            };
+        }
+
+        function getContent(){
+            return fread($this->filepointer, filesize());
         }
 
     }
