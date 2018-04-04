15ec57dfe13893bdadd8c86fc996f7537977e515
fogelkuba <fogelkuba@gmail.com>
2018-04-04 21:55:33 +0200

    fix

--- kurs-sql/rekordy/index.php
+++ kurs-sql/rekordy/index.php
@@ -14,27 +14,27 @@
     $password = "root";
     $dbname = "kursmysql";
 
-    // Create connection
-    $conn = mysqli_connect($servername, $username, $password, $dbname);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
+    // Create conection
+    $con = mysqli_connect($servername, $username, $password, $dbname);
+    // Check conection
+    if (!$con) {
+        die("conection failed: " . mysqli_connect_error());
     }
 
-    mysqli_select_db($conn, 'klienci');
-    mysqli_set_charset($conn, 'utf8');
+    mysqli_select_db($con, 'klienci');
+    mysqli_set_charset($con, 'utf8');
 
     $query = "
         INSERT INTO klienci
         (name, surname, age)
 
-        VALUE
-        ('test', 'test', 12)
+        VALUES
+        ('test2', 'test2', 22)
     ";
-    mysqli_query($query) or die(mysqli_error($con));
+    mysqli_query($con, $query) or die(mysqli_error($con));
 
 
-    mysqli_close($conn);
+    mysqli_close($con);
 ?>
 </body>
 </html>
