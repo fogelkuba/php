f7f28fb6bbecfa53af6d220cb706fa96cd090073
fogelkuba <fogelkuba@gmail.com>
2018-04-04 22:45:55 +0200

    fix

--- kurs-sql/usuwanie/index.php
+++ kurs-sql/usuwanie/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Usuwanie</title>
+    <title>Tabele</title>
 </head>
 <body>
 
@@ -24,21 +24,15 @@
     mysqli_select_db($con, 'klienci');
     mysqli_set_charset($con, 'utf8');
 
-    // $name = 'pjplhplgfplpflyf-drop-database';
-    // $name = filter_var($name, FILTER_SANITIZE_STRING);
-    //
-    // $query = "
-    //     INSERT INTO klienci (name, surname, age)
-    //     VALUES ('$name', 'test2', 22)
-    // ";
-    // mysqli_query($con, $query) or die(mysqli_error($con));
+    $name = 'pjplhplgfplpflyf-drop-database';
+    $name = filter_var($name, FILTER_SANITIZE_STRING);
 
     $query = "
-        DELETE FROM klienci
-        WHERE
-        login = '' AND pass = ''
+        INSERT INTO klienci (name, surname, age)
+        VALUES ('$name', 'test2', 22)
     ";
     mysqli_query($con, $query) or die(mysqli_error($con));
+
 
     mysqli_close($con);
 ?>
