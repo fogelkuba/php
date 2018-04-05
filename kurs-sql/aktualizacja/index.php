159cbe72db9400a90f27dfbb739c393101538037
fogelkuba <fogelkuba@gmail.com>
2018-04-05 21:22:30 +0200

    fix

--- kurs-sql/aktualizacja/index.php
+++ kurs-sql/aktualizacja/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Usuwanie</title>
+    <title>aktualizacja</title>
 </head>
 <body>
 
@@ -24,14 +24,6 @@
     mysqli_select_db($con, 'klienci');
     mysqli_set_charset($con, 'utf8');
 
-    // $name = 'pjplhplgfplpflyf-drop-database';
-    // $name = filter_var($name, FILTER_SANITIZE_STRING);
-    //
-    // $query = "
-    //     INSERT INTO klienci (name, surname, age)
-    //     VALUES ('$name', 'test2', 22)
-    // ";
-    // mysqli_query($con, $query) or die(mysqli_error($con));
 
     // $query = "
     //     DELETE FROM klienci
@@ -39,7 +31,11 @@
     //     login = '' AND pass = ''
     // ";
 
-    
+    $query= "
+        UPDATE klienci SET name = 'ana' WHERE  login = 'Szymon'
+        ";
+
+
     mysqli_query($con, $query) or die(mysqli_error($con));
 
     mysqli_close($con);
